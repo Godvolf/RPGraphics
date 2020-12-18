73edb144787d2cf88e1bee0ac086ba9e542c7a83
Godvolf <adam.howaniec@gmail.com>
2020-12-18 14:59:40 +0100

    enable both libs to work together

--- src/components/Clmtrackr/Clmtrackr.jsx
+++ src/components/Clmtrackr/Clmtrackr.jsx
@@ -21,7 +21,7 @@ export default function Clmtrackr() {
         ctrack.start(video);
         function drawLoop() {
             requestAnimationFrame(drawLoop);
-            overlayCC.clearRect(0, 0, 400, 300);
+           // overlayCC.clearRect(0, 0, 640, 480);
             ctrack.draw(overlay);
             }
             drawLoop();
